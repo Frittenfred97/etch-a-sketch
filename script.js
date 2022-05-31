c6581725758adb30146d3a9bd5b370695c3ae52f
Frederic Carboneras <94573933+Frittenfred97@users.noreply.github.com>
2022-05-31 17:00:19 +0200

    added Rainbow&Eraser

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let sliderInput = document.getElementById("sliderInput");
 let sliderOutput = document.getElementById("sliderOutput");
 let drawArea = document.getElementById("drawArea");
 let buttonWhite = document.getElementById("white");
+let buttonRainbow = document.getElementById("rainbow");
+let buttonErase = document.getElementById("eraser");
 
 
 sliderOutput.textContent = `${sliderInput.value}x${sliderInput.value}`;
@@ -15,14 +17,12 @@ sliderInput.oninput = function() {
 }
 let sliderValue = sliderInput.value;
 
-
+//function that creates squares based on the input value from the slider
 function createSquares () {
 
-
-
     let drawArea = document.getElementById("drawArea");
+    const parent = document.getElementById("drawArea");
 
-    const parent = document.getElementById("drawArea")
     while (parent.firstChild) {
     parent.firstChild.remove()
     }
@@ -34,16 +34,16 @@ function createSquares () {
         column.style.height = 30 / sliderValue + "em";
         column.style.width = 30 / sliderValue + "em";
     }
-
 };
 
-createSquares();
 
-sliderInput.addEventListener("input", () => {
-    createSquares();
-} );
+function randomColor() {
+    let random = Math.floor(Math.random()*16777215).toString(16);
+    let color = "#" + random;
+    return color;
+}
 
-
+//function that changes color of Square to white when hovering
 function colorWhite() {
 
     let whiteColor = document.getElementsByClassName("square");
@@ -52,10 +52,39 @@ function colorWhite() {
           whiteColor[i].onmouseover = (e) => {
               e.target.style.backgroundColor = "white";
           };
-      };
-  };
+    };
+};
 
-buttonWhite.addEventListener("click", colorWhite)
+//function that erases color of Square when hovering
+function colorErase() {
 
+    let eraseColor = document.getElementsByClassName("square");
 
-// add mouseOver CSS Class.
+    for (i = 0; i < eraseColor.length; i++) {
+          eraseColor[i].onmouseover = (e) => {
+              e.target.style.backgroundColor = "#3E3054";
+          };
+    };
+};
+
+//function that changes color of Square randomly when hovering
+  function colorRainbow() {
+
+    let rainbowColor = document.getElementsByClassName("square");
+
+    for (i = 0; i < rainbowColor.length; i++) {
+          rainbowColor[i].onmouseover = (e) => {
+              e.target.style.backgroundColor = randomColor();
+          };
+    };
+};
+
+createSquares();
+
+sliderInput.addEventListener("input", createSquares);
+
+buttonWhite.addEventListener("click", colorWhite);
+
+buttonRainbow.addEventListener("click", colorRainbow);
+
+buttonErase.addEventListener("click", colorErase);
